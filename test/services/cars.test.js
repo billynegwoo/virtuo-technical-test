74f5259688b908a145641ceeefbb93837c33f506
Kevin Blondel <kevin3.blondel@epitech.eu>
2019-05-16 16:35:01 +0200

    scafolding tests

--- test/services/cars.test.js
+++ test/services/cars.test.js
@@ -1,10 +1,56 @@
 const assert = require('assert');
 const app = require('../../src/app');
+const url = require('url');
+
+const port = app.get('port') || 3030;
+const getUrl = pathname => url.format({
+  hostname: app.get('host') || 'localhost',
+  protocol: 'http',
+  port,
+  pathname
+});
 
 describe('\'cars\' service', () => {
+  before(function(done) {
+    this.server = app.listen(port);
+    this.server.once('listening', () => done());
+  });
+
+  after(function(done) {
+    this.server.close(done);
+  });
+
   it('registered the service', () => {
     const service = app.service('cars');
-
     assert.ok(service, 'Registered the service');
   });
+
+  it('should create a car without errors', () => {
+
+  });
+
+  it('should not create a car and return a error', () => {
+
+  });
+
+  it('should not update a car and return a error', () => {
+
+  });
+
+  it('should update without errors', () => {
+
+  });
+
+  it('should list all cars', () => {
+
+  });
+
+  it('should get a car', () => {
+
+  });
+
+  it('should delete a car', () => {
+
+  });
+
 });
--- test/services/stations.test.js
+++ test/services/stations.test.js
@@ -1,10 +1,58 @@
 const assert = require('assert');
 const app = require('../../src/app');
+const url = require('url');
+
+const port = app.get('port') || 3030;
+const getUrl = pathname => url.format({
+  hostname: app.get('host') || 'localhost',
+  protocol: 'http',
+  port,
+  pathname
+});
 
 describe('\'stations\' service', () => {
+
+  before(function(done) {
+    this.server = app.listen(port);
+    this.server.once('listening', () => done());
+  });
+
+  after(function(done) {
+    this.server.close(done);
+  });
+
   it('registered the service', () => {
     const service = app.service('stations');
 
     assert.ok(service, 'Registered the service');
   });
+
+  it('should create a station without errors', () => {
+
+  });
+
+  it('should not create a station and return a error', () => {
+
+  });
+
+  it('should not update a station and return a error', () => {
+
+  });
+
+  it('should update a station without errors', () => {
+
+  });
+
+  it('should list all stations', () => {
+
+  });
+
+  it('should get a station', () => {
+
+  });
+
+  it('should delete a station', () => {
+
+  });
+
 });
