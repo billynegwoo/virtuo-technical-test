c9c48fc7c30505ffb392325030c9a025effd8247
Kevin Blondel <kevin3.blondel@epitech.eu>
2019-05-16 15:44:09 +0200

    creating cars service

---
+++ src/models/cars.model.js
@@ -0,0 +1,15 @@
+// cars-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const cars = new Schema({
+    text: { type: String, required: true }
+  }, {
+    timestamps: true
+  });
+
+  return mongooseClient.model('cars', cars);
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const stations = require('./stations/stations.service.js');
+const cars = require('./cars/cars.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(stations);
+  app.configure(cars);
 };
